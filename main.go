123eab33308b01e69a0e2c8bfa196112ad5b6bf8
yaront1111 <76696443+yaront1111@users.noreply.github.com>
2024-07-07 14:45:12 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-    "terraform-provider-bridge/bridge"
+    "github.com/bridge-yt/terraform-provider-bridge/bridge"
 )
 
 func main() {
